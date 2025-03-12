1f961c39bcc1713df81dc7be5e31e4b640dcccdc
Sushi <quatre101904@gmail.com>
2025-03-12 17:07:56 +0800

    updated the files

--- website/views.py
+++ website/views.py
@@ -118,3 +118,13 @@ def inventory(request):
 			return redirect('home')
 	else:
 		return render(request, 'inventory.html', {'inventorys':inventorys})
+
+
+def customer_inventory(request, pk):
+	if request.user.is_authenticated:
+		# Look Up Records
+		customer_inventory = Record.objects.get(id=pk)
+		return render(request, 'record.html', {'customer_inventory':customer_inventory})
+	else:
+		messages.success(request, "You Must Be Logged In To View That Page...")
+		return redirect('home')
