b2ac7a9c84459b9275c660a8a415c91da6a34ad0
Sushi <quatre101904@gmail.com>
2025-03-12 14:08:19 +0800

    updated the files

--- website/views.py
+++ website/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm, AddRecordForm
-from .models import Record, Inventory
+from .models import Record
 
 
 def home(request):
@@ -101,11 +101,20 @@ def update_record(request, pk):
 		return redirect('home')
 
 
-def inventory(request, pk):
-	if request.user.is_authenticated:
-		# Look Up Records
-		Inventory = Record.objects.get(id=pk)
-		return render(request, 'inventory.html', {'form':form})
+def inventory(request):
+	records = Record.objects.all()
+	# Check to see if logging in
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
+		# Authenticate
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			messages.success(request, "You Have Been Logged In!")
+			return redirect('home')
+		else:
+			messages.success(request, "There Was An Error Logging In, Please Try Again...")
+			return redirect('home')
 	else:
-		messages.success(request, "You Must Be Logged In To View That Page...")
-		return redirect('home')
+		return render(request, 'home.html', {'records':records})
