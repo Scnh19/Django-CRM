d8f1162320f8481dc69747c563cb85cbabd0cf4f
Sushi <quatre101904@gmail.com>
2025-03-12 17:39:32 +0800

    updated the files

--- website/views.py
+++ website/views.py
@@ -74,7 +74,7 @@ def delete_record(request, pk):
 
 
 def add_record(request):
-	form = AddRecordForm, AddInventoryForm(request.POST or None)
+	form = AddRecordForm(request.POST or None)
 	if request.user.is_authenticated:
 		if request.method == "POST":
 			if form.is_valid():
