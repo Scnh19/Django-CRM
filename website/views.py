0b69938602529e8cb669610ed27951cd154cfbb3
Sushi <quatre101904@gmail.com>
2025-03-12 16:49:08 +0800

    updated the files

--- website/views.py
+++ website/views.py
@@ -101,7 +101,7 @@ def update_record(request, pk):
 		return redirect('home')
 
 
-def inventory(request):
+def inventory(request, pk):
 	inventorys = Inventory.objects.all()
 	# Check to see if logging in
 	if request.method == 'POST':
